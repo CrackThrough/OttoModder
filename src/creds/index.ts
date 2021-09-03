06efe754f4c6397458b4ba66e72a141389d24222
CrackThrough <crackthrough@gmail.com>
2021-09-03 10:59:27 +0900

    Delete unnecessary file

--- src/creds/index.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from "fs";
-
-// Read credentials file and return an object
-const credentials = JSON.parse(readFileSync("./credentials.json").toString());
-
-// Export the credentials
-export { credentials };
